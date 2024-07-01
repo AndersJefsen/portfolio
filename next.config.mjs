c9a5e3f6352020d16cc32c01a3fc95a2cd1f078c
Anders Jefsen <andersjeff397@gmail.com>
2024-07-01 13:39:59 +0200

    chore: Update next.config.js for portfolio deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add your configuration options here
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
-
